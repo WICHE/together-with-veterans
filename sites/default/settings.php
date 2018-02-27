38c95cb9dacbf752d164b5744146c38e1e1bb1d8
WICHEwebDev <webdev@wiche.edu>
2018-02-27 15:39:39 -0700

    drush, settings.php & .gitignore

---
+++ sites/default/settings.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * PHP settings:
+ *
+ * To see what PHP settings are possible, including whether they can
+ * be set at runtime (ie., when ini_set() occurs), read the PHP
+ * documentation at http://www.php.net/manual/en/ini.php#ini.list
+ * and take a look at the .htaccess file to see which non-runtime
+ * settings are used there. Settings defined here should not be
+ * duplicated there so as to avoid conflict issues.
+ */
+ini_set('arg_separator.output',     '&amp;');
+ini_set('magic_quotes_runtime',     0);
+ini_set('magic_quotes_sybase',      0);
+ini_set('session.cache_expire',     200000);
+ini_set('session.cache_limiter',    'none');
+ini_set('session.cookie_lifetime',  2000000);
+ini_set('session.gc_maxlifetime',   200000);
+ini_set('session.save_handler',     'user');
+ini_set('session.use_only_cookies', 1);
+ini_set('session.use_trans_sid',    0);
+ini_set('url_rewriter.tags',        '');
+
+# Enables Clean urls
+$conf['clean_url'] = 1; // 1 enables, 0 clears clean_url
+
+$conf['drupal_http_request_fails'] = FALSE;
+
+if(getenv('AMAZEEIO_SITENAME')) {
+  $databases['default']['default'] = array(
+    'driver' => 'mysql',
+    'database' => getenv('AMAZEEIO_SITENAME'),
+    'username' => getenv('AMAZEEIO_DB_USERNAME'),
+    'password' => getenv('AMAZEEIO_DB_PASSWORD'),
+    'host' => getenv('AMAZEEIO_DB_HOST'),
+    'port' => getenv('AMAZEEIO_DB_PORT'),
+    'prefix' => '',
+  );
+}
+
+### amazee.io Varnish & reverse proxy settings
+if (getenv('AMAZEEIO_VARNISH_HOSTS') && getenv('AMAZEEIO_VARNISH_SECRET')) {
+  $varnish_hosts = explode(',', getenv('AMAZEEIO_VARNISH_HOSTS'));
+  array_walk($varnish_hosts, function(&$value, $key) { $value .= ':6082'; });
+
+  $conf['reverse_proxy'] = TRUE;
+  $conf['reverse_proxy_addresses'] = array_merge(explode(',', getenv('AMAZEEIO_VARNISH_HOSTS')), array('127.0.0.1'));
+  $conf['varnish_control_terminal'] = implode($varnish_hosts, " ");
+  $conf['varnish_control_key'] = getenv('AMAZEEIO_VARNISH_SECRET');
+  $conf['varnish_version'] = 4;
+}
+
+### Base URL
+if (getenv('AMAZEEIO_BASE_URL')) {
+	$base_url = getenv('AMAZEEIO_BASE_URL');
+}
+
+### Temp directory
+if (getenv('AMAZEEIO_TMP_PATH')) {
+  $conf['file_temporary_path'] = getenv('AMAZEEIO_TMP_PATH');
+}
+
+// Loading settings for all environment types.
+if (file_exists(__DIR__ . '/all.settings.php')) {
+  include __DIR__ . '/all.settings.php';
+}
+
+// Environment specific settings files.
+if(getenv('AMAZEEIO_SITE_ENVIRONMENT')){
+  if (file_exists(__DIR__ . '/' . getenv('AMAZEEIO_SITE_ENVIRONMENT') . '.settings.php')) {
+    include __DIR__ . '/' . getenv('AMAZEEIO_SITE_ENVIRONMENT') . '.settings.php';
+  }
+}
+
+// Last: this servers specific settings files.
+if (file_exists(__DIR__ . '/settings.local.php')) {
+  include __DIR__ . '/settings.local.php';
+}
