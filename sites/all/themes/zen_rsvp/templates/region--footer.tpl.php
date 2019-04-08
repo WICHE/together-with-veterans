895b3f6a816482a149f09bb18f2a97a429078272
WICHEwebDev <webdev@wiche.edu>
2019-04-08 10:02:49 -0600

    update views module, update hard-coded logos in region-footer.tpl.php

--- sites/all/themes/zen_rsvp/templates/region--footer.tpl.php
+++ sites/all/themes/zen_rsvp/templates/region--footer.tpl.php
@@ -9,14 +9,16 @@
 ?>
 <?php if ($content): ?>
   <footer id="footer" class="<?php print $classes; ?>" style="padding:12px;">
-  <a href="http://www.mirecc.va.gov/visn19" target="_blank"><img src="/sites/all/themes/zen_rsvp/images/mirecc-logo.png" style="float:left; max-width:134px; margin-right:6%;margin-bottom:16px;" alt="Rocky Mountain MIRECC (Mental Illness Research, Education and Clinical Center)" /></a>
+  <a href="http://www.mirecc.va.gov/visn19" target="_blank"><img src="/sites/all/themes/zen_rsvp/images/mirecc-logo.png" style="float:left; max-width:134px; margin-right:6%;margin-left:4%;margin-bottom:16px;" alt="Rocky Mountain MIRECC (Mental Illness Research, Education and Clinical Center)" /></a>
   
    <a href="http://www.va.gov" target="_blank"><img src="/sites/all/themes/zen_rsvp/images/VA_Seal_Logo.png" style="float:left; max-width:250px; margin-right:6%;margin-bottom:16px;" alt="Veteran's Affairs Seal | U.S Department of Veterans Affiars" /></a>
+	  
+	  
+	<a href="https://www.ruralhealth.va.gov/" target="_blank"><img src="/sites/all/themes/zen_rsvp/images/VA_ORH_ColorSeal_black.png" style="float:left;width:100%; max-width:300px; margin-right:5%;margin-bottom:16px;" alt="U.S Department of Veterans Affairs, Veterans Health Administration Office of Rural Health" /></a>
    
-    <a href="http://wiche.edu" target="_blank"><img src="/sites/all/themes/zen_rsvp/images/wiche-logo.png" style="float:left;width:100%; max-width:185px; margin-right:5%;margin-top:10px;margin-bottom:16px;" alt="Western Interstate Commission for Higher Education" /></a>
+    <a href="http://wiche.edu" target="_blank"><img src="/sites/all/themes/zen_rsvp/images/wiche-logo.png" style="float:left;width:100%; max-width:185px; margin-right:5%;margin-bottom:16px;" alt="Western Interstate Commission for Higher Education" /></a>
     
-
-    <div style="float:right;"><?php print $content; ?></div>
+    <div style="clear:left;width:100%;text-align:center;"><?php print $content; ?></div>
     <div style="clear:both;"></div>
   </footer>
 <?php endif; ?>
